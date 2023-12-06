080812ca3527c278a9ebb8d8407396627bbe8f02
Gustavo <gustavolego09@gmail.com>
2023-12-06 20:00:47 -0300

    Meu primeiro repositorio remoto

---
+++ principal.js
@@ -0,0 +1,12 @@
+var banner = 
+    ["os melhores do Brasil!", "Qualidade e preço baixo!"];
+
+var bannerAtual = 0;
+
+function trocarBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector(`h2#mensagem`).textContent = 
+        banner[bannerAtual];
+}
+
+setInterval(trocarBanner, 2000);
